e271aa120e83569bf10acbaffff307b2c31a85b6
Diwakar110 <diwakar19072004@gmail.com>
2024-09-09 14:34:38 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,12 @@
+# app.py
+from flask import Flask
+
+#Welcome 
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return 'Welcome to my web application!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
